07de8b57d36de7b02a19159eaf783ea716e0413d
Mohit <mohitt.tech@gmail.com>
2025-11-29 22:46:45 +0530

    Update speed.py

--- NumpyArray-VS-PythonLists/speed.py
+++ NumpyArray-VS-PythonLists/speed.py
@@ -1,11 +1,10 @@
 # Numpy array vs Python lists
 
 # python's lists
-a = [i for i in range(10000000)]
-b = [i for i in range(10000000,20000000)]
+list1 = [i for i in range(10000000)]
+list2 = [i for i in range(10000000,20000000)]
 
 c = []
 
 for i in range(len(a)):
     c.append(a[i] + b[i])
-    
